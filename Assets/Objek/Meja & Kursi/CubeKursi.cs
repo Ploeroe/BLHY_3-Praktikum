096132f53f5eb92fb89ec36452822c4eda777414
Ploeroe <raphael.hakim@student.umn.ac.id>
2022-10-18 23:50:39 +0700

    Fix Some Error in Vertices

--- Assets/Objek/Meja & Kursi/CubeKursi.cs
+++ Assets/Objek/Meja & Kursi/CubeKursi.cs
@@ -11,7 +11,7 @@ public class CubeKursi : MonoBehaviour
     void Start()
     {
         Mesh mesh = new Mesh();
-        var kursi = new Vector3[46];
+        var kursi = new Vector3[48];
 
         //Badan Kursi Bagian Belakang
         kursi[0] = new Vector3(2f,0f,2f);
@@ -26,60 +26,60 @@ public class CubeKursi : MonoBehaviour
         kursi[7] = new Vector3(-2f,0f,-2f);
     
         //Senderan Kursi Bagian Belakang
-        kursi[8] = new Vector3(-2f,2f,-2f);
+        kursi[8] = new Vector3(2f,0f,-2f); // 4
         kursi[9] = new Vector3(2f,2f,-2f);
+        kursi[10] = new Vector3(-2f,2f,-2f);
+        kursi[11] = new Vector3(-2f,0f,-2f); // 7
 
         //Senderan Kursi Bagian Depan
-        kursi[10] = new Vector3(-2f,2f,-1f);
-        kursi[11] = new Vector3(-2f,0f,-1f);
-        kursi[12] = new Vector3(2f,0f,-1f);
-        kursi[13] = new Vector3(2f,2f,-1f);
+        kursi[12] = new Vector3(-2f,2f,-1f);
+        kursi[13] = new Vector3(-2f,0f,-1f);
+        kursi[14] = new Vector3(2f,0f,-1f);
+        kursi[15] = new Vector3(2f,2f,-1f);
 
         //Kaki Kursi Bagian Belakang Kiri
-        // kursi[14] = new Vector3(-2f,-1f,-2f);//6
-        kursi[14] = new Vector3(-2f,-2f,-2f);
-        kursi[15] = new Vector3(-1f,-2f,-2f);
-        kursi[16] = new Vector3(-1f,-1f,-2f);
+        kursi[16] = new Vector3(-2f,-1f,-2f); // 6
+        kursi[17] = new Vector3(-2f,-2f,-2f);
+        kursi[18] = new Vector3(-1f,-2f,-2f);
+        kursi[19] = new Vector3(-1f,-1f,-2f);
 
-        kursi[17] = new Vector3(-2f,-1f,-1f);
-        kursi[18] = new Vector3(-2f,-2f,-1f);
-        kursi[19] = new Vector3(-1f,-2f,-1f);
-        kursi[20] = new Vector3(-1f,-1f,-1f);
-
+        kursi[20] = new Vector3(-2f,-1f,-1f);
+        kursi[21] = new Vector3(-2f,-2f,-1f);
+        kursi[22] = new Vector3(-1f,-2f,-1f);
+        kursi[23] = new Vector3(-1f,-1f,-1f);
 
         //Kaki Kursi Bagian Belakang Kanan
-        kursi[21] = new Vector3(1f,-1f,-2f);
-        kursi[22] = new Vector3(1f,-2f,-2f);
-        kursi[23] = new Vector3(2f,-2f,-2f);
-        // kursi[25] = new Vector3(2f,-1f,-2f);//5
+        kursi[24] = new Vector3(1f,-1f,-2f);
+        kursi[25] = new Vector3(1f,-2f,-2f);
+        kursi[26] = new Vector3(2f,-2f,-2f);
+        kursi[27] = new Vector3(2f,-1f,-2f);
 
-        kursi[24] = new Vector3(1f,-1f,-1f);
-        kursi[25] = new Vector3(1f,-2f,-1f);
-        kursi[26] = new Vector3(2f,-2f,-1f);
-        kursi[27] = new Vector3(2f,-1f,-1f);
+        kursi[28] = new Vector3(1f,-1f,-1f);
+        kursi[29] = new Vector3(1f,-2f,-1f);
+        kursi[30] = new Vector3(2f,-2f,-1f);
+        kursi[31] = new Vector3(2f,-1f,-1f);
 
         //Kaki Kursi Bagian Depan Kiri
-        kursi[28] = new Vector3(-2f,-1f,1f);
-        kursi[29] = new Vector3(-2f,-2f,1f);
-        kursi[30] = new Vector3(-1f,-2f,1f);
-        kursi[31] = new Vector3(-1f,-1f,1f);
+        kursi[32] = new Vector3(-2f,-1f,1f);
+        kursi[33] = new Vector3(-2f,-2f,1f);
+        kursi[34] = new Vector3(-1f,-2f,1f);
+        kursi[35] = new Vector3(-1f,-1f,1f);
 
-        // kursi[34] = new Vector3(-2f,-1f,2f);2
-        kursi[32] = new Vector3(-2f,-2f,2f);
-        kursi[33] = new Vector3(-1f,-2f,2f);
-        kursi[34] = new Vector3(-1f,-1f,2f);
+        kursi[36] = new Vector3(-2f,-1f,2f);//2
+        kursi[37] = new Vector3(-2f,-2f,2f);
+        kursi[38] = new Vector3(-1f,-2f,2f);
+        kursi[39] = new Vector3(-1f,-1f,2f);
 
         //Kaki Kursi Bagian Depan Kanan
+        kursi[40] = new Vector3(1f,-1f,1f);
+        kursi[41] = new Vector3(1f,-2f,1f);
+        kursi[42] = new Vector3(2f,-2f,1f);
+        kursi[43] = new Vector3(2f,-1f,1f);
 
-        kursi[35] = new Vector3(1f,-1f,1f);
-        kursi[36] = new Vector3(1f,-2f,1f);
-        kursi[37] = new Vector3(2f,-2f,1f);
-        kursi[38] = new Vector3(2f,-1f,1f);
-
-        kursi[39] = new Vector3(1f,-1f,2f);
-        kursi[40] = new Vector3(1f,-2f,2f);
-        kursi[41] = new Vector3(2f,-2f,2f);
-        // kursi[45] = new Vector3(2f,-1f,2f);//1
+        kursi[44] = new Vector3(1f,-1f,2f);
+        kursi[45] = new Vector3(1f,-2f,2f);
+        kursi[46] = new Vector3(2f,-2f,2f);
+        kursi[47] = new Vector3(2f,-1f,2f);//1
 
         mesh.vertices = kursi;
 
@@ -97,61 +97,70 @@ public class CubeKursi : MonoBehaviour
             1, 5, 4,
             4, 0, 1,
 
-            4,7,9,
-            7,8,9,
-            7,11,8,
-            11,10,8,
-            11,12,10,
-            12,13,10,
-            12,4,13,
-            4,9,13,
-            9,8,13,
-            8,10,13,
+            8,11,9,
+            11,10,9,
+            11,13,10,
+            13,12,10,
+            13,14,12,
+            14,15,12,
+            14,8,15,
+            8,9,15,
+            9,10,15,
+            10,12,15,
+            14,13,8,
+            13,11,8,
 
-            15,14,16,
-            14,6,16,
-            14,18,6,
-            18,17,6,
-            18,19,17,
-            19,20,17,
-            19,15,20,
-            15,16,20,
-            19,18,15,
-            18,14,15,
+            18,17,19,
+            17,16,19,
+            17,21,16,
+            21,20,16,
+            21,22,20,
+            22,23,20,
+            22,18,23,
+            18,19,23,
+            22,21,18,
+            21,17,18,
+            19,16,23,
+            16,20,23,
 
-            23,22,5,
-            22,21,5,
-            22,25,21,
-            25,24,21,
-            25,26,24,
-            26,27,24,
-            26,23,27,
-            23,5,27,
-            26,25,23,
-            25,22,23,
+            26,25,27,
+            25,24,27,
+            25,29,24,
+            29,28,24,
+            29,30,28,
+            30,31,28,
+            30,26,31,
+            26,27,31,
+            30,29,26,
+            29,25,26,
+            27,24,31,
+            24,28,31,
 
-            30,29,31,
-            29,28,31,
-            29,32,28,
-            32,2,28,
-            32,33,2,
-            33,34,2,
-            33,30,34,
-            30,31,34,
-            33,32,30,
-            32,29,30,
+            34,33,35,
+            33,32,35,
+            33,37,32,
+            37,36,32,
+            37,38,36,
+            38,39,36,
+            38,34,39,
+            34,35,39,
+            38,37,34,
+            37,33,34,
+            35,32,39,
+            32,36,39,
 
-            37,36,38,
-            36,35,38,
-            36,40,35,
-            40,39,35,
-            40,41,39,
-            41,1,39,
-            41,37,1,
-            37,38,1,
-            41,40,37,
-            40,36,37,
-
+            42,41,43,
+            41,40,43,
+            41,45,40,
+            45,44,40,
+            45,46,44,
+            46,47,44,
+            46,42,47,
+            42,43,47,
+            46,45,42,
+            45,41,42,
+            43,40,47,
+            40,44,47
         };
         GetComponent<MeshFilter>().mesh = mesh;
 
--- Assets/Objek/Monitor/CubeLayar.cs
+++ Assets/Objek/Monitor/CubeLayar.cs
@@ -5,55 +5,175 @@ using UnityEngine;
 public class CubeLayar : MonoBehaviour
 {
     [SerializeField]
-    public Material cubeMaterial;
+    public Material CubeLayarMaterial;
 
     // Declaring and initializing values for our mesh
     float width = 0.4f;
     float height = 7.0f;
     float thick = 15.0f;
 
+    Texture CubeLayarTexture;
+
     // Start is called before the first frame update
-    void Start()
-    {
-        Mesh mesh = new Mesh();
-        var baseMonitor = new Vector3[8];
-
-        //First Layer of 4-baseMonitor Quad
-        baseMonitor[0] = new Vector3(-width, -height, thick);
-        baseMonitor[1] = new Vector3(-width, height, thick);
-        baseMonitor[2] = new Vector3(width, height, thick);
-        baseMonitor[3] = new Vector3(width, -height, thick);
-
-        //Second Layer of 4-baseMonitor Quad
-        baseMonitor[4] = new Vector3(-width, -height, -thick);
-        baseMonitor[5] = new Vector3(-width, height, -thick);
-        baseMonitor[6] = new Vector3(width, height, -thick);
-        baseMonitor[7] = new Vector3(width, -height, -thick);
+   void Start()
+    {   
     
-        mesh.vertices = baseMonitor;
+    Mesh mesh = new Mesh();
+    var vertices = new Vector3[24];
+    var uvs = new Vector2[vertices.Length];
 
-        mesh.triangles = new int[]{
-            2, 1, 0,
-            3, 2, 0, //First Face
-            3, 0, 4,
-            4, 7, 3, //Second Face
-            3, 6, 2,
-            3, 7, 6, //Third Face
-            6, 5, 2,
-            5, 1, 2, //Fourth Face
-            5, 6, 4,
-            6, 7, 4, //Fifth Face
-            1, 5, 4,
-            4, 0, 1  //Sixth Face
+    CubeLayarTexture = Resources.Load<Texture>("Textures/Carpet");
+
+        vertices[0] = new Vector3(width, height, thick);
+        vertices[1] = new Vector3(width, -height, thick);
+        vertices[2] = new Vector3(width, height, -thick);
+        vertices[3] = new Vector3(width, -height, -thick);
+
+        uvs[0] = new Vector2(1.0f, 1.0f);
+        uvs[1] = new Vector2(1.0f, 0.0f);
+        uvs[2] = new Vector2(0.0f, 1.0f);
+        uvs[3] = new Vector2(0.0f, 0.0f);
+
+        vertices[4] = new Vector3(width, height, thick);
+        vertices[5] = new Vector3(-width, height, thick);
+        vertices[6] = new Vector3(width, height, -thick);
+        vertices[7] = new Vector3(-width, height, -thick);
+
+        uvs[4] = new Vector2(1.0f, 1.0f);
+        uvs[5] = new Vector2(1.0f, 0.0f);
+        uvs[6] = new Vector2(0.0f, 1.0f);
+        uvs[7] = new Vector2(0.0f, 0.0f);
+
+        vertices[8] = new Vector3(width, height, thick);
+        vertices[9] = new Vector3(-width, height, thick);
+        vertices[10] = new Vector3(width, -height, thick);
+        vertices[11] = new Vector3(-width, -height, thick);
+
+        uvs[8] = new Vector2(1.0f, 1.0f);
+        uvs[9] = new Vector2(1.0f, 0.0f);
+        uvs[10] = new Vector2(0.0f, 1.0f);
+        uvs[11] = new Vector2(0.0f, 0.0f);
+
+        vertices[12] = new Vector3(-width, height, thick);
+        vertices[13] = new Vector3(-width, -height, thick);
+        vertices[14] = new Vector3(-width, height, -thick);
+        vertices[15] = new Vector3(-width, -height, -thick);
+
+        uvs[12] = new Vector2(1.0f, 1.0f);
+        uvs[13] = new Vector2(1.0f, 0.0f);
+        uvs[14] = new Vector2(0.0f, 1.0f);
+        uvs[15] = new Vector2(0.0f, 0.0f);
+
+        vertices[16] = new Vector3(width, -height, thick);
+        vertices[17] = new Vector3(-width, -height, thick);
+        vertices[18] = new Vector3(width, -height, -thick);
+        vertices[19] = new Vector3(-width, -height, -thick);
+
+        uvs[16] = new Vector2(1.0f, 1.0f);
+        uvs[17] = new Vector2(1.0f, 0.0f);
+        uvs[18] = new Vector2(0.0f, 1.0f);
+        uvs[19] = new Vector2(0.0f, 0.0f);
+
+        vertices[20] = new Vector3(width, height, -thick);
+        vertices[21] = new Vector3(-width, height, -thick);
+        vertices[22] = new Vector3(width, -height, -thick);
+        vertices[23] = new Vector3(-width, -height, -thick);
+
+        uvs[20] = new Vector2(1.0f, 1.0f);
+        uvs[21] = new Vector2(1.0f, 0.0f);
+        uvs[22] = new Vector2(0.0f, 1.0f);
+        uvs[23] = new Vector2(0.0f, 0.0f);
+
+        mesh.vertices = vertices;
+
+        var colors = new Color32[vertices.Length];
+
+        colors[0] = new Color32(211, 211, 211, 255);
+        colors[1] = new Color32(211, 211, 211, 255);
+        colors[2] = new Color32(211, 211, 211, 255);
+        colors[3] = new Color32(211, 211, 211, 255);
+
+        colors[4] = new Color32(211, 211, 211, 255);
+        colors[5] = new Color32(211, 211, 211, 255);
+        colors[6] = new Color32(211, 211, 211, 255);
+        colors[7] = new Color32(211, 211, 211, 255);
+
+        colors[8] = new Color32(211, 211, 211, 255);
+        colors[9] = new Color32(211, 211, 211, 255);
+        colors[10] = new Color32(211, 211, 211, 255);
+        colors[11] = new Color32(211, 211, 211, 255);
+
+        colors[12] = new Color32(211, 211, 211, 255);
+        colors[13] = new Color32(211, 211, 211, 255);
+        colors[14] = new Color32(211, 211, 211, 255);
+        colors[15] = new Color32(211, 211, 211, 255);
+
+        colors[16] = new Color32(211, 211, 211, 255);
+        colors[17] = new Color32(211, 211, 211, 255);
+        colors[18] = new Color32(211, 211, 211, 255);
+        colors[19] = new Color32(211, 211, 211, 255);
+
+        colors[20] = new Color32(211, 211, 211, 255);
+        colors[21] = new Color32(211, 211, 211, 255);
+        colors[22] = new Color32(211, 211, 211, 255);
+        colors[23] = new Color32(211, 211, 211, 255);
+
+        mesh.colors32 = colors;
+        mesh.uv = uvs;
+
+        mesh.triangles = new int[] {
+            2,0,1,
+            2,1,3,
+
+            6,7,5,
+            4,6,5,
+
+            9,10,8,
+            9,11,10,
+
+            12,14,13,
+            14,15,13,
+
+            19,18,17,
+            18,16,17,
+
+            21,20,23,
+            20,22,23
         };
-        GetComponent<MeshFilter>().mesh = mesh;
 
-        GetComponent<MeshRenderer>().material = cubeMaterial;
-    }
+        mesh.normals = new Vector3[] {
+            new Vector3(1.0f , 0.0f, 0.0f),
+            new Vector3(1.0f , 0.0f, 0.0f),
+            new Vector3(1.0f , 0.0f, 0.0f),
+            new Vector3(1.0f , 0.0f, 0.0f),
+            
+            new Vector3(0.0f , 1.0f, 0.0f),
+            new Vector3(0.0f , 1.0f, 0.0f),
+            new Vector3(0.0f , 1.0f, 0.0f),
+            new Vector3(0.0f , 1.0f, 0.0f),
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+            new Vector3(0.0f , 0.0f, 1.0f),
+            new Vector3(0.0f , 0.0f, 1.0f),
+            new Vector3(0.0f , 0.0f, 1.0f),
+            new Vector3(0.0f , 0.0f, 1.0f),
+
+            new Vector3(-1.0f , 0.0f, 0.0f),
+            new Vector3(-1.0f , 0.0f, 0.0f),
+            new Vector3(-1.0f , 0.0f, 0.0f),
+            new Vector3(-1.0f , 0.0f, 0.0f),
+
+            new Vector3(0.0f , -1.0f, 0.0f),
+            new Vector3(0.0f , -1.0f, 0.0f),
+            new Vector3(0.0f , -1.0f, 0.0f),
+            new Vector3(0.0f , -1.0f, 0.0f),
+
+            new Vector3(0.0f , 0.0f, -1.0f),
+            new Vector3(0.0f , 0.0f, -1.0f),
+            new Vector3(0.0f , 0.0f, -1.0f),
+            new Vector3(0.0f , 0.0f, -1.0f),
+         };
+    mesh.RecalculateNormals();
+    GetComponent<MeshFilter>().mesh = mesh;
+    GetComponent<MeshRenderer>().material = CubeLayarMaterial;
     }
 }
