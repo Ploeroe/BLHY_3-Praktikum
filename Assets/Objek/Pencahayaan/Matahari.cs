3b5ccc65bde6ce593d4c086b20a6fa5eaf8c7028
lifosmin <65651883+lifosmin@users.noreply.github.com>
2022-10-18 15:59:02 +0700

    matahari done

---
+++ Assets/Objek/Pencahayaan/Matahari.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Matahari : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody>().angularVelocity = new Vector3(.1f, 0, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
