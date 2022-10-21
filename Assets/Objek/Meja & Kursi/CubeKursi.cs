aff540e1f7de76d6e1d43935e1103f402e374295
Ploeroe <raphael.hakim@student.umn.ac.id>
2022-10-21 17:53:02 +0700

    Assets, dan pasang texture meja n kursi

--- Assets/Objek/Meja & Kursi/CubeKursi.cs
+++ Assets/Objek/Meja & Kursi/CubeKursi.cs
@@ -5,13 +5,18 @@ using UnityEngine;
 public class CubeKursi : MonoBehaviour
 {
 [SerializeField]
-    public Material cubeMaterial;
+    public Material CubeKursiMaterial;
+
+    Texture CubeKursiTexture;
 
     // Start is called before the first frame update
     void Start()
     {
         Mesh mesh = new Mesh();
         var kursi = new Vector3[48];
+        var uvs = new Vector2[kursi.Length];
+
+        CubeKursiTexture = Resources.Load<Texture>("Textures/Kursi");
 
         //Badan Kursi Bagian Belakang
         kursi[0] = new Vector3(2f,0f,2f);
@@ -19,17 +24,32 @@ public class CubeKursi : MonoBehaviour
         kursi[2] = new Vector3(-2f,-1f,2f);
         kursi[3] = new Vector3(-2f,0f,2f);
 
+        uvs[0] = new Vector2(1.0f, 1.0f);
+        uvs[1] = new Vector2(1.0f, 0.0f);
+        uvs[2] = new Vector2(0.0f, 1.0f);
+        uvs[3] = new Vector2(0.0f, 0.0f);
+
         //Badan Kursi Bagian Depan
         kursi[4] = new Vector3(2f,0f,-2f);
         kursi[5] = new Vector3(2f,-1f,-2f);
         kursi[6] = new Vector3(-2f,-1f,-2f);
         kursi[7] = new Vector3(-2f,0f,-2f);
-    
+
+        uvs[4] = new Vector2(1.0f, 1.0f);
+        uvs[5] = new Vector2(1.0f, 0.0f);
+        uvs[6] = new Vector2(0.0f, 1.0f);
+        uvs[7] = new Vector2(0.0f, 0.0f); 
+
         //Senderan Kursi Bagian Belakang
         kursi[8] = new Vector3(2f,0f,-2f); // 4
         kursi[9] = new Vector3(2f,2f,-2f);
         kursi[10] = new Vector3(-2f,2f,-2f);
         kursi[11] = new Vector3(-2f,0f,-2f); // 7
+
+        uvs[8] = new Vector2(1.0f, 1.0f);
+        uvs[9] = new Vector2(1.0f, 0.0f);
+        uvs[10] = new Vector2(0.0f, 1.0f);
+        uvs[11] = new Vector2(0.0f, 0.0f);
 
         //Senderan Kursi Bagian Depan
         kursi[12] = new Vector3(-2f,2f,-1f);
@@ -37,16 +57,31 @@ public class CubeKursi : MonoBehaviour
         kursi[14] = new Vector3(2f,0f,-1f);
         kursi[15] = new Vector3(2f,2f,-1f);
 
+        uvs[12] = new Vector2(1.0f, 1.0f);
+        uvs[13] = new Vector2(1.0f, 0.0f);
+        uvs[14] = new Vector2(0.0f, 1.0f);
+        uvs[15] = new Vector2(0.0f, 0.0f);
+
         //Kaki Kursi Bagian Belakang Kiri
         kursi[16] = new Vector3(-2f,-1f,-2f); // 6
         kursi[17] = new Vector3(-2f,-2f,-2f);
         kursi[18] = new Vector3(-1f,-2f,-2f);
         kursi[19] = new Vector3(-1f,-1f,-2f);
 
+        uvs[16] = new Vector2(1.0f, 1.0f);
+        uvs[17] = new Vector2(1.0f, 0.0f);
+        uvs[18] = new Vector2(0.0f, 1.0f);
+        uvs[19] = new Vector2(0.0f, 0.0f);
+
         kursi[20] = new Vector3(-2f,-1f,-1f);
         kursi[21] = new Vector3(-2f,-2f,-1f);
         kursi[22] = new Vector3(-1f,-2f,-1f);
         kursi[23] = new Vector3(-1f,-1f,-1f);
+
+        uvs[20] = new Vector2(1.0f, 1.0f);
+        uvs[21] = new Vector2(1.0f, 0.0f);
+        uvs[22] = new Vector2(0.0f, 1.0f);
+        uvs[23] = new Vector2(0.0f, 0.0f);
 
         //Kaki Kursi Bagian Belakang Kanan
         kursi[24] = new Vector3(1f,-1f,-2f);
@@ -54,10 +89,20 @@ public class CubeKursi : MonoBehaviour
         kursi[26] = new Vector3(2f,-2f,-2f);
         kursi[27] = new Vector3(2f,-1f,-2f);
 
+        uvs[24] = new Vector2(1.0f, 1.0f);
+        uvs[25] = new Vector2(1.0f, 0.0f);
+        uvs[26] = new Vector2(0.0f, 1.0f);
+        uvs[27] = new Vector2(0.0f, 0.0f);
+
         kursi[28] = new Vector3(1f,-1f,-1f);
         kursi[29] = new Vector3(1f,-2f,-1f);
         kursi[30] = new Vector3(2f,-2f,-1f);
         kursi[31] = new Vector3(2f,-1f,-1f);
+
+        uvs[28] = new Vector2(1.0f, 1.0f);
+        uvs[29] = new Vector2(1.0f, 0.0f);
+        uvs[30] = new Vector2(0.0f, 1.0f);
+        uvs[31] = new Vector2(0.0f, 0.0f);
 
         //Kaki Kursi Bagian Depan Kiri
         kursi[32] = new Vector3(-2f,-1f,1f);
@@ -65,10 +110,20 @@ public class CubeKursi : MonoBehaviour
         kursi[34] = new Vector3(-1f,-2f,1f);
         kursi[35] = new Vector3(-1f,-1f,1f);
 
+        uvs[32] = new Vector2(1.0f, 1.0f);
+        uvs[33] = new Vector2(1.0f, 0.0f);
+        uvs[34] = new Vector2(0.0f, 1.0f);
+        uvs[35] = new Vector2(0.0f, 0.0f);
+
         kursi[36] = new Vector3(-2f,-1f,2f);//2
         kursi[37] = new Vector3(-2f,-2f,2f);
         kursi[38] = new Vector3(-1f,-2f,2f);
         kursi[39] = new Vector3(-1f,-1f,2f);
+
+        uvs[36] = new Vector2(1.0f, 1.0f);
+        uvs[37] = new Vector2(1.0f, 0.0f);
+        uvs[38] = new Vector2(0.0f, 1.0f);
+        uvs[39] = new Vector2(0.0f, 0.0f);
 
         //Kaki Kursi Bagian Depan Kanan
         kursi[40] = new Vector3(1f,-1f,1f);
@@ -76,12 +131,87 @@ public class CubeKursi : MonoBehaviour
         kursi[42] = new Vector3(2f,-2f,1f);
         kursi[43] = new Vector3(2f,-1f,1f);
 
+        uvs[40] = new Vector2(1.0f, 1.0f);
+        uvs[41] = new Vector2(1.0f, 0.0f);
+        uvs[42] = new Vector2(0.0f, 1.0f);
+        uvs[43] = new Vector2(0.0f, 0.0f);
+
         kursi[44] = new Vector3(1f,-1f,2f);
         kursi[45] = new Vector3(1f,-2f,2f);
         kursi[46] = new Vector3(2f,-2f,2f);
         kursi[47] = new Vector3(2f,-1f,2f);//1
 
+        uvs[44] = new Vector2(1.0f, 1.0f);
+        uvs[45] = new Vector2(1.0f, 0.0f);
+        uvs[46] = new Vector2(0.0f, 1.0f);
+        uvs[47] = new Vector2(0.0f, 0.0f);
+
         mesh.vertices = kursi;
+
+        var colors = new Color32[kursi.Length];
+
+        colors[0] = new Color32(211, 211, 211, 255);
+        colors[1] = new Color32(211, 211, 211, 255);
+        colors[2] = new Color32(211, 211, 211, 255);
+        colors[3] = new Color32(211, 211, 211, 255);
+
+        colors[4] = new Color32(211, 211, 211, 255);
+        colors[5] = new Color32(211, 211, 211, 255);
+        colors[6] = new Color32(211, 211, 211, 255);
+        colors[7] = new Color32(211, 211, 211, 255);
+
+        colors[8] = new Color32(211, 211, 211, 255);
+        colors[9] = new Color32(211, 211, 211, 255);
+        colors[10] = new Color32(211, 211, 211, 255);
+        colors[11] = new Color32(211, 211, 211, 255);
+
+        colors[12] = new Color32(211, 211, 211, 255);
+        colors[13] = new Color32(211, 211, 211, 255);
+        colors[14] = new Color32(211, 211, 211, 255);
+        colors[15] = new Color32(211, 211, 211, 255);
+
+        colors[16] = new Color32(211, 211, 211, 255);
+        colors[17] = new Color32(211, 211, 211, 255);
+        colors[18] = new Color32(211, 211, 211, 255);
+        colors[19] = new Color32(211, 211, 211, 255);
+
+        colors[20] = new Color32(211, 211, 211, 255);
+        colors[21] = new Color32(211, 211, 211, 255);
+        colors[22] = new Color32(211, 211, 211, 255);
+        colors[23] = new Color32(211, 211, 211, 255);
+
+        colors[24] = new Color32(211, 211, 211, 255);
+        colors[25] = new Color32(211, 211, 211, 255);
+        colors[26] = new Color32(211, 211, 211, 255);
+        colors[27] = new Color32(211, 211, 211, 255);
+
+        colors[28] = new Color32(211, 211, 211, 255);
+        colors[29] = new Color32(211, 211, 211, 255);
+        colors[30] = new Color32(211, 211, 211, 255);
+        colors[31] = new Color32(211, 211, 211, 255);
+
+        colors[32] = new Color32(211, 211, 211, 255);
+        colors[33] = new Color32(211, 211, 211, 255);
+        colors[34] = new Color32(211, 211, 211, 255);
+        colors[35] = new Color32(211, 211, 211, 255);
+
+        colors[36] = new Color32(211, 211, 211, 255);
+        colors[37] = new Color32(211, 211, 211, 255);
+        colors[38] = new Color32(211, 211, 211, 255);
+        colors[39] = new Color32(211, 211, 211, 255);
+
+        colors[40] = new Color32(211, 211, 211, 255);
+        colors[41] = new Color32(211, 211, 211, 255);
+        colors[42] = new Color32(211, 211, 211, 255);
+        colors[43] = new Color32(211, 211, 211, 255);
+
+        colors[44] = new Color32(211, 211, 211, 255);
+        colors[45] = new Color32(211, 211, 211, 255);
+        colors[46] = new Color32(211, 211, 211, 255);
+        colors[47] = new Color32(211, 211, 211, 255);
+
+        mesh.colors32 = colors;
+        mesh.uv = uvs;
 
         mesh.triangles = new int[]{
             2, 1, 0,
@@ -162,9 +292,9 @@ public class CubeKursi : MonoBehaviour
             43,40,47,
             40,44,47
         };
+        mesh.RecalculateNormals();
         GetComponent<MeshFilter>().mesh = mesh;
-
-        GetComponent<MeshRenderer>().material = cubeMaterial;
+        GetComponent<MeshRenderer>().material = CubeKursiMaterial;
     }
 
     // Update is called once per frame
--- Assets/Objek/Meja & Kursi/CubeMeja.cs
+++ Assets/Objek/Meja & Kursi/CubeMeja.cs
@@ -5,13 +5,17 @@ using UnityEngine;
 public class CubeMeja : MonoBehaviour
 {
 [SerializeField]
-    public Material cubeMaterial;
+    public Material CubeMejaMaterial;
 
+    Texture CubeMejaTexture;
     // Start is called before the first frame update
     void Start()
     {
         Mesh mesh = new Mesh();
         var meja = new Vector3[46];
+        var uvs = new Vector2[meja.Length];
+
+        CubeMejaTexture = Resources.Load<Texture>("Textures/Meja");
 
         //Badan Meja
         meja[0] = new Vector3(2f,1f,2f);
@@ -19,21 +23,41 @@ public class CubeMeja : MonoBehaviour
         meja[2] = new Vector3(-2f,0f,2f);
         meja[3] = new Vector3(-2f,1f,2f);
 
+        uvs[0] = new Vector2(1.0f, 1.0f);
+        uvs[1] = new Vector2(1.0f, 0.0f);
+        uvs[2] = new Vector2(0.0f, 1.0f);
+        uvs[3] = new Vector2(0.0f, 0.0f);
+
         meja[4] = new Vector3(2f,1f,-2f);
         meja[5] = new Vector3(2f,0f,-2f);
         meja[6] = new Vector3(-2f,0f,-2f);
         meja[7] = new Vector3(-2f,1f,-2f);
-        
+
+        uvs[4] = new Vector2(1.0f, 1.0f);
+        uvs[5] = new Vector2(1.0f, 0.0f);
+        uvs[6] = new Vector2(0.0f, 1.0f);
+        uvs[7] = new Vector2(0.0f, 0.0f); 
+
         //Kaki Meja Bagian Belakang Kiri
         meja[8] = new Vector3(-2f,0f,-2f);
         meja[9] = new Vector3(-2f,-2f,-2f);
         meja[10] = new Vector3(-1f,-2f,-2f);
         meja[11] = new Vector3(-1f,0f,-2f);
 
+        uvs[8] = new Vector2(1.0f, 1.0f);
+        uvs[9] = new Vector2(1.0f, 0.0f);
+        uvs[10] = new Vector2(0.0f, 1.0f);
+        uvs[11] = new Vector2(0.0f, 0.0f);
+
         meja[12] = new Vector3(-2f,0f,-1f);
         meja[13] = new Vector3(-2f,-2f,-1f);
         meja[14] = new Vector3(-1f,-2f,-1f);
         meja[15] = new Vector3(-1f,0f,-1f);
+
+        uvs[12] = new Vector2(1.0f, 1.0f);
+        uvs[13] = new Vector2(1.0f, 0.0f);
+        uvs[14] = new Vector2(0.0f, 1.0f);
+        uvs[15] = new Vector2(0.0f, 0.0f);
 
         //Kaki Meja Bagian Belakang Kanan
         meja[16] = new Vector3(1f,0f,-2f);
@@ -41,21 +65,41 @@ public class CubeMeja : MonoBehaviour
         meja[18] = new Vector3(2f,-2f,-2f);
         meja[19] = new Vector3(2f,0f,-2f);
 
+        uvs[16] = new Vector2(1.0f, 1.0f);
+        uvs[17] = new Vector2(1.0f, 0.0f);
+        uvs[18] = new Vector2(0.0f, 1.0f);
+        uvs[19] = new Vector2(0.0f, 0.0f);
+
         meja[20] = new Vector3(1f,0f,-1f);
         meja[21] = new Vector3(1f,-2f,-1f);
         meja[22] = new Vector3(2f,-2f,-1f);
         meja[23] = new Vector3(2f,0f,-1f);
+
+        uvs[20] = new Vector2(1.0f, 1.0f);
+        uvs[21] = new Vector2(1.0f, 0.0f);
+        uvs[22] = new Vector2(0.0f, 1.0f);
+        uvs[23] = new Vector2(0.0f, 0.0f);
 
         //Kaki Meja Bagian Depan Kiri
         meja[24] = new Vector3(-2f,0f,1f);
         meja[25] = new Vector3(-2f,-2f,1f);
         meja[26] = new Vector3(-1f,-2f,1f);
         meja[27] = new Vector3(-1f,0f,1f);
-        
+
+        uvs[24] = new Vector2(1.0f, 1.0f);
+        uvs[25] = new Vector2(1.0f, 0.0f);
+        uvs[26] = new Vector2(0.0f, 1.0f);
+        uvs[27] = new Vector2(0.0f, 0.0f);
+
         meja[28] = new Vector3(-2f,0f,2f);
         meja[29] = new Vector3(-2f,-2f,2f);
         meja[30] = new Vector3(-1f,-2f,2f);
         meja[31] = new Vector3(-1f,0f,2f);
+
+        uvs[28] = new Vector2(1.0f, 1.0f);
+        uvs[29] = new Vector2(1.0f, 0.0f);
+        uvs[30] = new Vector2(0.0f, 1.0f);
+        uvs[31] = new Vector2(0.0f, 0.0f);
 
         //Kaki Meja Bagian Depan Kanan
         meja[32] = new Vector3(1f,0f,1f);
@@ -63,12 +107,77 @@ public class CubeMeja : MonoBehaviour
         meja[34] = new Vector3(2f,-2f,1f);
         meja[35] = new Vector3(2f,0f,1f);
 
+        uvs[32] = new Vector2(1.0f, 1.0f);
+        uvs[33] = new Vector2(1.0f, 0.0f);
+        uvs[34] = new Vector2(0.0f, 1.0f);
+        uvs[35] = new Vector2(0.0f, 0.0f);
+
         meja[36] = new Vector3(1f,0f,2f);
         meja[37] = new Vector3(1f,-2f,2f);
         meja[38] = new Vector3(2f,-2f,2f);
         meja[39] = new Vector3(2f,0f,2f);
 
+        uvs[36] = new Vector2(1.0f, 1.0f);
+        uvs[37] = new Vector2(1.0f, 0.0f);
+        uvs[38] = new Vector2(0.0f, 1.0f);
+        uvs[39] = new Vector2(0.0f, 0.0f);
+
         mesh.vertices = meja;
+
+        var colors = new Color32[meja.Length];
+
+        colors[0] = new Color32(211, 211, 211, 255);
+        colors[1] = new Color32(211, 211, 211, 255);
+        colors[2] = new Color32(211, 211, 211, 255);
+        colors[3] = new Color32(211, 211, 211, 255);
+
+        colors[4] = new Color32(211, 211, 211, 255);
+        colors[5] = new Color32(211, 211, 211, 255);
+        colors[6] = new Color32(211, 211, 211, 255);
+        colors[7] = new Color32(211, 211, 211, 255);
+
+        colors[8] = new Color32(211, 211, 211, 255);
+        colors[9] = new Color32(211, 211, 211, 255);
+        colors[10] = new Color32(211, 211, 211, 255);
+        colors[11] = new Color32(211, 211, 211, 255);
+
+        colors[12] = new Color32(211, 211, 211, 255);
+        colors[13] = new Color32(211, 211, 211, 255);
+        colors[14] = new Color32(211, 211, 211, 255);
+        colors[15] = new Color32(211, 211, 211, 255);
+
+        colors[16] = new Color32(211, 211, 211, 255);
+        colors[17] = new Color32(211, 211, 211, 255);
+        colors[18] = new Color32(211, 211, 211, 255);
+        colors[19] = new Color32(211, 211, 211, 255);
+
+        colors[20] = new Color32(211, 211, 211, 255);
+        colors[21] = new Color32(211, 211, 211, 255);
+        colors[22] = new Color32(211, 211, 211, 255);
+        colors[23] = new Color32(211, 211, 211, 255);
+
+        colors[24] = new Color32(211, 211, 211, 255);
+        colors[25] = new Color32(211, 211, 211, 255);
+        colors[26] = new Color32(211, 211, 211, 255);
+        colors[27] = new Color32(211, 211, 211, 255);
+
+        colors[28] = new Color32(211, 211, 211, 255);
+        colors[29] = new Color32(211, 211, 211, 255);
+        colors[30] = new Color32(211, 211, 211, 255);
+        colors[31] = new Color32(211, 211, 211, 255);
+
+        colors[32] = new Color32(211, 211, 211, 255);
+        colors[33] = new Color32(211, 211, 211, 255);
+        colors[34] = new Color32(211, 211, 211, 255);
+        colors[35] = new Color32(211, 211, 211, 255);
+
+        colors[36] = new Color32(211, 211, 211, 255);
+        colors[37] = new Color32(211, 211, 211, 255);
+        colors[38] = new Color32(211, 211, 211, 255);
+        colors[39] = new Color32(211, 211, 211, 255);
+
+        mesh.colors32 = colors;
+        mesh.uv = uvs;
 
         mesh.triangles = new int[]{
             2, 1, 0,
@@ -128,9 +237,9 @@ public class CubeMeja : MonoBehaviour
             9,10,13,
             10,14,13,
         };
+        mesh.RecalculateNormals();
         GetComponent<MeshFilter>().mesh = mesh;
-
-        GetComponent<MeshRenderer>().material = cubeMaterial;
+        GetComponent<MeshRenderer>().material = CubeMejaMaterial;
     }
 
     // Update is called once per frame
